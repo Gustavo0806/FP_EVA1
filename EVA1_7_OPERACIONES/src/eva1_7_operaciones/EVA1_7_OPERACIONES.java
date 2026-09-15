aad6f4f622e8192033098bb541e866a1738619a6
Gustavo0806 <L26550248@chihuahua2.tecnm.mx>
2026-09-15 17:57:03 -0600

    Add files via upload

---
+++ EVA1_7_OPERACIONES/src/eva1_7_operaciones/EVA1_7_OPERACIONES.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_operaciones;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_7_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int resultado, val1 = 10, val2 =5;
+        resultado = 100 + val1 * (val1/val2);
+        System.out.print("resultado: ");
+        System.out.println(resultado);
+    }
+    
+}
